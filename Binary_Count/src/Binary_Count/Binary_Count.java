3dd263655f45335adf993ca3eaa46d2726d9fa8e
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-23 23:34:11 -0400

    Update Binary_Count.java

--- Binary_Count/src/Binary_Count/Binary_Count.java
+++ Binary_Count/src/Binary_Count/Binary_Count.java
@@ -1,4 +1,14 @@
 package Binary_Count;
 
 public class Binary_Count {
+  public int[] Binary_Count(int userInput){
+    int[] output = new int[userInput+1];
+    int initial = 0;
+    output[0] = initial;
+
+    for(int position = initial + 1; position <= userInput; position++){
+        output[position] = output[position & (position-1)] + 1;
+    }
+    return output;
+    }
 }
