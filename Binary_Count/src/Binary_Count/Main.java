8985fb8cb8b3cf922b0af2e06dde62c5bf222067
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-23 23:33:45 -0400

    Update Main.java

--- Binary_Count/src/Binary_Count/Main.java
+++ Binary_Count/src/Binary_Count/Main.java
@@ -2,6 +2,9 @@ package Binary_Count;
 
 public class Main {
     public static void main(String[] args){
+         Binary_Count BC = new Binary_Count();
+         int userInput = 2;
+         System.out.println("The output array will be: " + Arrays.toString(BC.Binary_Count(userInput)));
 
     }
 }
